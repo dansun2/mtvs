4aec7947cbd00271a23dc2d0988a9c827ddb80c0
dansun2 <dansunmusik7@gmail.com>
2025-03-14 09:53:51 +0900

    oop

---
+++ 04_java/Module02-control-flow-and-method/src/main/java/com/ohgiraffers/Baekjoon.java
@@ -0,0 +1,13 @@
+package com.ohgiraffers;
+
+import java.util.Scanner;
+
+public class Baekjoon {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a, b;
+        a = sc.nextInt();
+        b = sc.nextInt();
+        System.out.println(a + b);
+    }
+}
