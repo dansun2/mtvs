3b353f11539665aa242dead8cf8a81f266f70ace
dansun2 <dansunmusik7@gmail.com>
2025-04-21 10:30:43 +0900

    thymeleaf

---
+++ 10_spring-webMVC/chap08-thymeleaf/src/main/java/com/ohgiraffers/thymeleaf/config/Chap08ThymeleafApplication.java
@@ -0,0 +1,13 @@
+package com.ohgiraffers.thymeleaf.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackages = "com.ohgiraffers.thymeleaf")
+public class Chap08ThymeleafApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Chap08ThymeleafApplication.class, args);
+    }
+
+}
