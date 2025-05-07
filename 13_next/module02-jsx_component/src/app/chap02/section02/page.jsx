360573233825ec0acd2375c8101c80d9ced2ff64
dansun2 <dansunmusik7@gmail.com>
2025-05-07 16:59:51 +0900

    component

---
+++ 13_next/module02-jsx_component/src/app/chap02/section02/page.jsx
@@ -0,0 +1,7 @@
+const { default: FunctionComponent } = require("@/component/chap02/section02/FuntionComponent")
+
+const Section02 = () => {
+    return <FunctionComponent />
+}
+
+export default Section02;
