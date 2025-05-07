2350a3263ecec86397152ee598fd02075dcbda43
dansun2 <dansunmusik7@gmail.com>
2025-05-08 08:52:39 +0900

    state_component

---
+++ 13_next/module03-props_state/src/app/chap02/section01/page.jsx
@@ -0,0 +1,9 @@
+const { default: StateComponent } = require("@/component/chap02/section01/StateComponent");
+
+function Section01 () {
+    return (
+        <StateComponent />
+    )
+}
+
+export default Section01;
